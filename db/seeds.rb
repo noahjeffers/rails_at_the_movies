7238beca230a9d73f59421a7fcbe4a481fdd85dd
Noah Jeffers <njeffers@academic.rrc.ca>
2021-02-02 16:28:18 -0600

    end of day 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+Movie.delete_all
+ProductionCompany.delete_all
+
+filename = Rails.root.join("db/top_movies.csv")
+
+csv_data = File.read(filename)
+
+movies = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+movies.each do |m|
+  production_company = ProductionCompany.find_or_create_by(name: m["production_company"])
+
+  if production_company && production_company.valid?
+    # create a movie based on production company
+    movie = production_company.movies.create(
+      title:        m["original_title"],
+      year:         m["year"],
+      duration:     m["duration"],
+      description:  m["description"],
+      average_vote: m["avg_vote"]
+    )
+
+    puts "Invalid movie #{m['original_title']}" unless movie&.valid?
+  else
+    puts "Invalid Production Company #{m['production_company']} for movie #{m['original_title']}"
+  end
+end

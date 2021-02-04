6da342bea4b71c77b9b5b5dee72c81a7191367db
Noah Jeffers <njeffers@academic.rrc.ca>
2021-02-04 16:03:29 -0600

    end of thursdays class

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,6 +1,10 @@
 class ProductionCompaniesController < ApplicationController
   def index
-    @production_companies = ProductionCompany.all
+    @production_companies = ProductionCompany.select("production_companies.*")
+                                             .select("COUNT(production_companies.id) as movie_count")
+                                             .joins(:movies)
+                                             .group("production_companies.id ")
+                                             .order("movie_count DESC")
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :movies, only: [:index, :show]
-  resources :production_companies, only: [:index, :show]
+  resources :movies, only: %i[index show]
+  resources :production_companies, only: %i[index show]
+
+  root to: "home#index"
 end

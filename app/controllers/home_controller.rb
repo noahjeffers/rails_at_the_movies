86a0eb5481423979129f3522cc58acf9887255de
Noah Jeffers <njeffers@academic.rrc.ca>
2021-02-09 15:24:28 -0600

    Refactored Production Company Model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @movies = Movie.includes(:production_company)
                    .order("average_vote DESC").limit(10)
-    @production_companies = ProductionCompany.limit(10)
+    @production_companies = ProductionCompany.ordered_by_movies.limit(10)
   end
 end
--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,10 +1,6 @@
 class ProductionCompaniesController < ApplicationController
   def index
-    @production_companies = ProductionCompany.select("production_companies.*")
-                                             .select("COUNT(production_companies.id) as movie_count")
-                                             .joins(:movies)
-                                             .group("production_companies.id ")
-                                             .order("movie_count DESC")
+    @production_companies = ProductionCompany.ordered_by_movies
   end
 
   def show

d7fbf8ffc18b9584c50aa30d4af60a441ada7144
Noah Jeffers <njeffers@academic.rrc.ca>
2021-02-11 15:32:05 -0600

    added genres and moviegenres

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,9 @@
 class Movie < ApplicationRecord
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   belongs_to :production_company
+
+  # validation
   validates :title, uniqueness: true
   validates :title, :year, :duration, :description, :average_vote, presence: true
   validates :year, :duration, numericality: { only_integer: true }

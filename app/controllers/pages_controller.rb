4c0da14f27aa2b8caf1da65f17de6de128eedc14
Noah Jeffers <njeffers@academic.rrc.ca>
2021-02-11 14:35:24 -0600

    updated Pages. changed show to Permalink

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  before_action :set_page, only: %i[ show edit update destroy ]
+  http_basic_authenticate_with name: "admin", password: "password", except: [:permalink]
+  before_action :set_page, only: %i[permalink edit update destroy]
 
   # GET /pages or /pages.json
   def index
@@ -7,7 +8,8 @@ class PagesController < ApplicationController
   end
 
   # GET /pages/1 or /pages/1.json
-  def show
+  def permalink
+    @page = Page.find_by(permalink: params[:permalink])
   end
 
   # GET /pages/new
@@ -16,8 +18,7 @@ class PagesController < ApplicationController
   end
 
   # GET /pages/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /pages or /pages.json
   def create
@@ -26,7 +27,7 @@ class PagesController < ApplicationController
     respond_to do |format|
       if @page.save
         format.html { redirect_to @page, notice: "Page was successfully created." }
-        format.json { render :show, status: :created, location: @page }
+        format.json { render :permalink, status: :created, location: @page }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @page.errors, status: :unprocessable_entity }
@@ -39,7 +40,7 @@ class PagesController < ApplicationController
     respond_to do |format|
       if @page.update(page_params)
         format.html { redirect_to @page, notice: "Page was successfully updated." }
-        format.json { render :show, status: :ok, location: @page }
+        format.json { render :permalink, status: :ok, location: @page }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @page.errors, status: :unprocessable_entity }
@@ -57,13 +58,14 @@ class PagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_page
-      @page = Page.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def page_params
-      params.require(:page).permit(:title, :content, :permalink)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_page
+    @page = Page.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def page_params
+    params.require(:page).permit(:title, :content, :permalink)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :pages
+  resources :pages, except: [:show]
+  get "/pages/:permalink" => "pages#permalink", as: "permalink"
+
   resources :movies, only: %i[index show]
   resources :production_companies, only: %i[index show]
 

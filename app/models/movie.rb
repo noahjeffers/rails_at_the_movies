eb80a683cdaed6e053310d2c1cc1200af82989fe
Noah Jeffers <njeffers@academic.rrc.ca>
2021-02-11 15:55:09 -0600

    Added genres to movie view

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,4 +8,8 @@ class Movie < ApplicationRecord
   validates :title, :year, :duration, :description, :average_vote, presence: true
   validates :year, :duration, numericality: { only_integer: true }
   validates :average_vote, numericality: true
+
+  def genres_list
+    genres.map(&:name).join(", ")
+  end
 end
